041baf1a924809251fd49f2f1d867b9adbf703b3
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2025-07-19 18:35:08 +0300

    fix(install.sh): check if systemctl --user is avaliable

--- install.sh
+++ install.sh
@@ -15,13 +15,23 @@ check_sudo() {
 # Run script
 check_sudo
 
+echo "installing binaries to $INSTALL_DIR"
+sudo cp "${SCRIPT_DIR}"/{vlt,vltd} "$INSTALL_DIR"
+
+if ! command -v systemctl &>/dev/null; then
+    echo "error: systemctl not found in PATH" >&2
+    exit 1
+fi
+
+if ! systemctl --user show-environment &>/dev/null; then
+    echo "error: systemctl --user is not available or not supported in this environment" >&2
+    exit 1
+fi
+
 if systemctl --user is-active --quiet vltd; then
     echo "stopping running vltd service"
     systemctl --user stop vltd
 fi
-
-echo "installing binaries to $INSTALL_DIR"
-sudo cp "${SCRIPT_DIR}"/{vlt,vltd} "$INSTALL_DIR"
 
 echo "installing systemd unit to $SYSTEMD_DIR"
 mkdir -p "$SYSTEMD_DIR"
