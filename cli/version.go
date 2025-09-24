0aa70d0f8e06b3ea87e5e56ad8870787ef4e973e
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2025-09-25 00:33:28 +0300

    fix: disallow flags for the version cmd

--- cli/version.go
+++ cli/version.go
@@ -1,14 +1,33 @@
 package cli
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+
+	"github.com/ladzaretti/vlt-cli/clierror"
+	"github.com/ladzaretti/vlt-cli/genericclioptions"
+
+	"github.com/spf13/cobra"
+)
 
 func newVersionCommand(defaults *DefaultVltOptions) *cobra.Command {
-	return &cobra.Command{
-		Use:   "version",
-		Short: "Show version",
-		RunE: func(_ *cobra.Command, _ []string) error {
-			defaults.Printf("%s\n", Version)
-			return nil
+	cmd := cobra.Command{
+		Use:                "version",
+		Short:              "Show version",
+		DisableFlagParsing: true,
+		RunE: func(_ *cobra.Command, args []string) error {
+			return clierror.Check(func() error {
+				if len(args) > 0 {
+					return errors.New("version: command takes no arguments")
+				}
+
+				defaults.Printf("%s\n", Version)
+
+				return nil
+			}())
 		},
 	}
+
+	genericclioptions.MarkAllFlagsHidden(&cmd)
+
+	return &cmd
 }
